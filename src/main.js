f41a63113a569130f633cd035215ff348af8c952
Deepak <ds262969@gmail.com>
2025-11-22 07:39:20 +0530

    Added the basic vertex and fragment shaders in the icosahedron geometry

--- src/main.js
+++ src/main.js
@@ -1,24 +1,54 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import vertexShader from "../shaders/vertex.glsl";
+import fragmentShader from "../shaders/fragment.glsl";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Get canvas
+const canvas = document.getElementById("canvas");
 
-setupCounter(document.querySelector('#counter'))
+// Create renderer
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Create scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  20,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.z = 14;
+
+// Add Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+// Create Icosahedron geometry and shader material
+const geometry = new THREE.IcosahedronGeometry(2, 20, 20);
+const material = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+  wireframe: true,
+  side: THREE.DoubleSide,
+});
+const sphere = new THREE.Mesh(geometry, material);
+sphere.position.y = -2.5;
+scene.add(sphere);
+
+// Handle resizing
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
